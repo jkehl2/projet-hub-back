24302493163cd34a0af717b2aa292acd815161a0
julien-dosiere <juliendosiere@yahoo.fr>
2021-01-22 22:32:33 +0100

    heroku init

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const app = express();
 const redis = require('redis');
 const cors = require('cors');
 
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+
 cache.flushAll();
 
 
