2174262b681ea1be2dc02f7f9d2068f876d534ba
julien-dosiere <juliendosiere@yahoo.fr>
2021-01-24 10:57:07 +0100

    scripts git sync

---
+++ gitSync+heroku.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#params
+submodule="back"
+# Definining Commit name
+echo What is your commit name?
+read commitName
+# Sync submodule repo
+git add .
+git commit -m "$commitName"
+git push
+git push heroku
+# Sync parent repo
+cd ..
+git add .
+git commit -m "$commitName"
+git push
+cd $submodule
+
+echo "----- sync completed ------"
