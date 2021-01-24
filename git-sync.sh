57f3b9b01508930298987993ac798955146f7c96
julien-dosiere <juliendosiere@yahoo.fr>
2021-01-24 10:50:08 +0100

    test

--- git-sync.sh
+++ git-sync.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+#params
+submodule="back"
+# Definining Commit name
 echo What is your commit name?
 read commitName
+# Sync submodule repo
 git add .
 git commit -m "$commitName"
 git push
+# Sync parent repo
+cd ..
+git add .
+git commit -m "$commitName"
+git push
+cd $submodule
 
-echo "sync completed"
+echo "----- sync completed ------"
